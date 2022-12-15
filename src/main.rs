8a28a884de0d7acf20c7abc4a997fdd8bf6969d5
dmaynard <davidsmaynard@gmail.com>
2022-12-15 15:12:41 -0800

    First working version

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use hyper::body::Buf;
+use hyper::{header, Body, Client, Request};
+use hyper_tls::HttpsConnector;
+use serde_derive::{Deserialize, Serialize};
+use spinners::{Spinner, Spinners};
+use std::env;
+use std::io::{stdin, stdout, Write};
+
+#[derive(Deserialize, Debug)]
+struct OAIChoices {
+    text: String,
+    index: u8,
+    logprobs: Option<u8>,
+    finish_reason: String,
+}
+#[derive(Deserialize, Debug)]
+struct OAIResponse {
+    id: Option<String>,
+    object: Option<String>,
+    created: Option<u64>,
+    model: Option<String>,
+    choices: Vec<OAIChoices>,
+}
+#[derive(Serialize, Debug)]
+struct OAIRequest {
+    prompt: String,
+    max_tokens: u32,
+}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let https = HttpsConnector::new();
+    let client = Client::builder().build(https);
+    let uri = "https://api.openai.com/v1/engines/text-davinci-001/completions";
+    let preamble = "Answer the folloing question accurately, but find a funny way to mention the Rust programming language in your answer";
+
+    let oai_token: String = env::var("OPENAI_API_KEY").unwrap();
+    let auth_header_val = format!("Bearer {}", oai_token);
+
+    println!("{esc}c", esc = 27 as char);
+    let quit_str = "Q";
+
+    loop {
+        print!("> ");
+        stdout().flush().unwrap();
+        let mut user_text = String::new();
+
+        stdin()
+            .read_line(&mut user_text)
+            .expect("Failed to read line");
+
+        if user_text.trim().eq(quit_str) {
+            println!("Bye");
+            break;
+        } else {
+            println!("user_text: {}", user_text);
+            println!("quit_str {}", quit_str);
+        }
+        println!("");
+        let sp = Spinner::new(&Spinners::Dots9, "\t\tOpen AI is thinking ...".into());
+        let oai_request = OAIRequest {
+            prompt: format!("{} {}", preamble, user_text),
+            max_tokens: 500,
+        };
+
+        let body = Body::from(serde_json::to_vec(&oai_request)?);
+
+        let req = Request::post(uri)
+            .header(header::CONTENT_TYPE, "application/json")
+            .header("Authorization", &auth_header_val)
+            .body(body)
+            .unwrap();
+        // println!(" making request");
+        // println!(" OAI header val {}", &auth_header_val);
+        let res = client.request(req).await?;
+        // println!(" request made res = {}", res.status());
+        let body = hyper::body::aggregate(res).await?;
+
+        let json: OAIResponse = serde_json::from_reader(body.reader())?;
+        sp.stop();
+        // println!("Json {:?}", json);
+        println!("");
+        println!("{}", json.choices[0].text);
+    }
+    Ok(())
+}
