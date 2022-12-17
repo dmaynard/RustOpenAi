d91a031724e1ee2df0668a369bd8c356d9e3d957
dmaynard <davidsmaynard@gmail.com>
2022-12-16 17:51:37 -0800

    workking towards using different preludes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use hyper::body::Buf;
 use hyper::{header, Body, Client, Request};
 use hyper_tls::HttpsConnector;
+use rand::Rng;
 use serde_derive::{Deserialize, Serialize};
 use spinners::{Spinner, Spinners};
 use std::env;
+use std::f32::consts::E;
 use std::fs::File;
 use std::io::{stdin, stdout, Read, Write};
 #[derive(Serialize, Deserialize, Debug)]
@@ -37,7 +39,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let https = HttpsConnector::new();
     let client = Client::builder().build(https);
     let uri = "https://api.openai.com/v1/engines/text-davinci-001/completions";
-    let preamble = "Answer the folloing question accurately, but find a funny way to shame republicans or praise democrats in your answer";
+    let preamble = "Answer the following question accurately, but find a funny way to shame republicans or praise democrats in your answer";
 
     let oai_token: String = env::var("OPENAI_API_KEY").unwrap();
     let auth_header_val = format!("Bearer {}", oai_token);
@@ -45,13 +47,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let mut panelists_json = String::new();
     file.read_to_string(&mut panelists_json)?;
     let panelists: Vec<Panelist> = serde_json::from_str(&panelists_json)?;
-
-    for p in panelists {
-        println!("{:#?}", p);
-    }
+    let panel_size: i32 = panelists.len().try_into().unwrap();
 
     // println!("{esc}c", esc = 27 as char);
-    let quit_str = "Q";
+    println!(" Welcome to our Question and Answer Chat");
+    println!("Today we have {} distinguishe panelists", panelists.len());
+    println!("They are");
+    let mut index = 1;
+    for i in [0..4] {
+        println!("{}: {} ", index, panelists[i][0].name);
+        index = index + 1;
+    }
+    println!(
+        "Begin your question with a number or name to ask a specific panelist or Quit to exit"
+    );
+
+    let quit_str = "QUIT";
 
     loop {
         print!("> ");
@@ -61,11 +72,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         stdin()
             .read_line(&mut user_text)
             .expect("Failed to read line");
-
-        if user_text.to_uppercase().trim().eq(quit_str) {
+        let first_word = &user_text.split(" ").next().unwrap();
+        println!("first word is {}", first_word);
+        if first_word.to_uppercase().trim().eq(quit_str) {
             println!("Bye");
             break;
         }
+        let index = match first_word.parse::<i32>() {
+            Ok(n) => n,
+            Err(_e) => {
+                let mut rng = rand::thread_rng();
+                rng.gen_range(0..panel_size)
+            }
+        };
+        println!(" index = {}", index as usize);
+        let panelist = &panelists[index as usize];
         println!("");
         let mut sp = Spinner::new(Spinners::SimpleDots, "\t\tOpen AI is thinking ...".into());
         let oai_request = OAIRequest {
@@ -89,7 +110,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let json: OAIResponse = serde_json::from_reader(body.reader())?;
         sp.stop();
         // println!("Json {:?}", json);
-        println!("{}", json.choices[0].text);
+        println!("{}: {}", panelist.name, json.choices[0].text);
     }
     Ok(())
 }
